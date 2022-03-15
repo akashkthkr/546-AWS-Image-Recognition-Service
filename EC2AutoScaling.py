bdfd572d07a32babee69143d19fa8ee39bceb114
Ayush Kalani <kalani.ayush@gmail.com>
2022-03-15 02:29:11 -0700

    fixes1

--- EC2AutoScaling.py
+++ EC2AutoScaling.py
@@ -29,18 +29,46 @@ def create_key_pair():
       print("Key pair already exists")
     else:
         print("Unexpected error: %s" % e)
-    
+        
+def create_security_group():
+  response = ec2_client.create_security_group(
+    Description='string',
+    GroupName='string',
+    VpcId='string',
+    TagSpecifications=[
+        { 'ResourceType': 'export-image-task',
+         'Tags': [
+                {
+                    'Key': 'cloud-test-1',
+                    'Value': 'cloud-test-1'
+                },
+            ]
+        },
+    ],
+    DryRun=False
+  )
 
-def create_instance(sec_group_ids=["sg-0bb98ead470e1d287"], instance_type='t2.micro', min_count=1, max_count=1):
+def create_instance(min_count=1, max_count=1):
   try:
     logging.debug("Creating new instance")
     instance = ec2_res.create_instances(
         ImageId=AMI,
         MinCount=min_count,
         MaxCount=max_count,
-        InstanceType=instance_type,
-        SecurityGroupIds=sec_group_ids,
-        UserData=USER_DATA
+        InstanceType=constants.INSTANCE_TYPE,
+        SecurityGroupIds=constants.SECURITY_GROUP_ID,
+        UserData=USER_DATA,
+        TagSpecifications=[
+        {
+            'ResourceType': 'capacity-reservation'|'client-vpn-endpoint'|'customer-gateway'|'carrier-gateway'|'dedicated-host'|'dhcp-options'|'egress-only-internet-gateway'|'elastic-ip'|'elastic-gpu'|'export-image-task'|'export-instance-task'|'fleet'|'fpga-image'|'host-reservation'|'image'|'import-image-task'|'import-snapshot-task'|'instance'|'instance-event-window'|'internet-gateway'|'ipam'|'ipam-pool'|'ipam-scope'|'ipv4pool-ec2'|'ipv6pool-ec2'|'key-pair'|'launch-template'|'local-gateway'|'local-gateway-route-table'|'local-gateway-virtual-interface'|'local-gateway-virtual-interface-group'|'local-gateway-route-table-vpc-association'|'local-gateway-route-table-virtual-interface-group-association'|'natgateway'|'network-acl'|'network-interface'|'network-insights-analysis'|'network-insights-path'|'network-insights-access-scope'|'network-insights-access-scope-analysis'|'placement-group'|'prefix-list'|'replace-root-volume-task'|'reserved-instances'|'route-table'|'security-group'|'security-group-rule'|'snapshot'|'spot-fleet-request'|'spot-instances-request'|'subnet'|'subnet-cidr-reservation'|'traffic-mirror-filter'|'traffic-mirror-session'|'traffic-mirror-target'|'transit-gateway'|'transit-gateway-attachment'|'transit-gateway-connect-peer'|'transit-gateway-multicast-domain'|'transit-gateway-route-table'|'volume'|'vpc'|'vpc-endpoint'|'vpc-endpoint-service'|'vpc-peering-connection'|'vpn-connection'|'vpn-gateway'|'vpc-flow-log',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'app-tier-'
+                },
+            ]
+        },
+    ],
     )
     logging.debug("Instance created id:", instance["Instances"][0]["InstanceId"])
   except ClientError as e:
