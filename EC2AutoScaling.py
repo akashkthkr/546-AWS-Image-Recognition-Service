65a103e1ab40e5466b4cfcee180fd19bb9bf3d04
Akash Kant <thakur.akashkant@gmail.com>
2022-03-16 02:58:25 -0700

    Third Commit : app tier basic all methodologies except calling ami and code and constants and EC2 methods calls

--- EC2AutoScaling.py
+++ EC2AutoScaling.py
@@ -15,93 +15,102 @@ yum install git -y
 ec2_client = boto3.client('ec2', region_name=constants.REGION_NAME)
 ec2_res = boto3.resource('ec2', region_name=constants.REGION_NAME)
 
+
 def create_key_pair():
-  try:
-    key_pair = ec2_client.create_key_pair(KeyName=EC2_KEY_NAME)
-    private_key = key_pair["KeyMaterial"]
-    # write private key to file with 400 permissions
-    with os.fdopen(os.open("/tmp/aws_ec2_key.pem", os.O_WRONLY | os.O_CREAT, 0o400), "w+") as handle:
-      handle.write(private_key)
-  except ClientError as e:
-    if e.response['Error']['Code'] == 'EntityAlreadyExists':
-      print("Key pair already exists")
-    else:
-        print("Unexpected error: %s" % e)
-        
+    try:
+        key_pair = ec2_client.create_key_pair(KeyName=EC2_KEY_NAME)
+        private_key = key_pair["KeyMaterial"]
+        # write private key to file with 400 permissions
+        with os.fdopen(os.open("/tmp/aws_ec2_key.pem", os.O_WRONLY | os.O_CREAT, 0o400), "w+") as handle:
+            handle.write(private_key)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'EntityAlreadyExists':
+            print("Key pair already exists")
+        else:
+            print("Unexpected error: %s" % e)
+
+
 def create_security_group():
-  response = ec2_client.create_security_group(
-    Description='string',
-    GroupName='string',
-    VpcId='string',
-    TagSpecifications=[
-        { 'ResourceType': 'export-image-task',
-         'Tags': [
-                {
-                    'Key': 'cloud-test-1',
-                    'Value': 'cloud-test-1'
-                },
-            ]
-        },
-    ],
-    DryRun=False
-  )
+    response = ec2_client.create_security_group(
+        Description='string',
+        GroupName='string',
+        VpcId='string',
+        TagSpecifications=[
+            {'ResourceType': 'export-image-task',
+             'Tags': [
+                 {
+                     'Key': 'cloud-test-1',
+                     'Value': 'cloud-test-1'
+                 },
+             ]
+             },
+        ],
+        DryRun=False
+    )
+
 
 def create_instance(min_count=1, max_count=1):
-  try:
-    logging.debug("Creating new instance")
-    instances = ec2_res.create_instances(
-        ImageId=constants.APP_TIER_AMI,
-        MinCount=min_count,
-        MaxCount=max_count,
-        InstanceType=constants.INSTANCE_TYPE,
-        SecurityGroupIds=constants.SECURITY_GROUP_ID,
-        UserData=USER_DATA
-    )
-    for instance in instances:
-      ec2_res.create_tags(Resources=[instance.id], Tags=[
-          {
-              'Key': 'Name',
-              'Value': "app-instance1",
-          },
-      ])
-  except ClientError as e:
-    print("Unexpected error: %s" % e)
+    try:
+        logging.debug("Creating new instance")
+        instances = ec2_res.create_instances(
+            ImageId=constants.APP_TIER_AMI,
+            MinCount=min_count,
+            MaxCount=max_count,
+            InstanceType=constants.INSTANCE_TYPE,
+            SecurityGroupIds=constants.SECURITY_GROUP_ID,
+            UserData=USER_DATA
+        )
+        for instance in instances:
+            ec2_res.create_tags(Resources=[instance.id], Tags=[
+                {
+                    'Key': 'Name',
+                    'Value': "app-instance1",
+                },
+            ])
+    except ClientError as e:
+        print("Unexpected error: %s" % e)
+
 
 def stop_instances(instance_ids):
     logging.debug("Stopping instance:", instance_ids)
     response = ec2_client.stop_instances(InstanceIds=instance_ids)
     logging.debug(response)
 
+
 def terminate_instance(instance_ids):
     logging.debug("Terminating instance:", instance_ids)
     response = ec2_client.terminate_instances(InstanceIds=instance_ids)
     logging.debug(response)
-    
-def get_instances_by_state(state = ['running']):
+
+
+def get_instances_by_state(state=None):
+    if state is None:
+        state = ['running']
     filters = [
         {
-          'Name': 'instance-state-name', 
-          'Values': state
-        }, 
+            'Name': 'instance-state-name',
+            'Values': state
+        },
         {
-          'Name': 'image-id',
-          'Values': [constants.APP_TIER_AMI]
+            'Name': 'image-id',
+            'Values': [constants.APP_TIER_AMI]
         }
     ]
     instances = ec2_res.instances.filter(Filters=filters)
     for instance in instances:
-      logging.debug(instance.id)
+        logging.debug(instance.id)
     return [instance.id for instance in instances]
 
+
 def auto_scale_instances():
-    queue_length = 3#SQSManagement.numberOfMessagesInQueue()
+    queue_length = 3  # SQSManagement.numberOfMessagesInQueue()
     logging.debug("Request queue length: %s", queue_length)
 
     if queue_length == 0:
         logging.debug("No Request in queue ****SCALE IN****, stopping all instances")
         # make sure the instance is idle and do not stop if it is currenlty processing a request
         stop_instances(get_instances_by_state())
-    else: 
+    else:
         number_of_running_instances = len(get_instances_by_state())
         logging.debug("Number of running instances: %s", number_of_running_instances)
         if number_of_running_instances >= MAX_LIMIT_INSTANCES:
@@ -114,22 +123,23 @@ def auto_scale_instances():
             stopped_instances_id_list = get_instances_by_state(['stopped'])
             num_of_stopped_instances = len(stopped_instances_id_list)
             logging.debug("Number of stopped_instances %s", num_of_stopped_instances)
-            
+
             new_instances_required = 0
             if total_instances_to_spawn - num_of_stopped_instances > 0:
-              new_instances_required = total_instances_to_spawn - num_of_stopped_instances
+                new_instances_required = total_instances_to_spawn - num_of_stopped_instances
             # Start the stopped instances, if any
             if num_of_stopped_instances > 0:
-              ec2_client.start_instances(InstanceIds=stopped_instances_id_list)
+                ec2_client.start_instances(InstanceIds=stopped_instances_id_list)
             # Start remainder new instances
             if new_instances_required > 0:
-              logging.debug(" Starting %s new instances", new_instances_required)
-              create_instance(min_count=new_instances_required, max_count=new_instances_required)
+                logging.debug(" Starting %s new instances", new_instances_required)
+                create_instance(min_count=new_instances_required, max_count=new_instances_required)
         # scale down
         if number_of_running_instances > queue_length:
-        # stop half of the instances
-          return
-            
+            # stop half of the instances
+            return
+
+
 while True:
     # test commit
     logging.debug("starting auto scaling")
---
+++ appTier.py
@@ -0,0 +1,126 @@
+import base64
+
+import boto3
+from botocore.exceptions import ClientError
+import logging
+import os
+import constants
+import EC2AutoScaling
+import SQSManagement
+
+# Constants
+S3_INPUT_BUCKET = constants.AWS_S3_INPUT_BUCKET_NAME
+S3_OUTPUT_BUCKET = constants.AWS_S3_OUTPUT_BUCKET_NAME
+
+SQS_REQUEST_QUEUE_NAME = constants.AWS_SQS_REQUEST_QUEUE_NAME
+SQS_RESPONSE_QUEUE_NAME = constants.AWS_SQS_RESPONSE_QUEUE_NAME
+
+# initialization and instantiations
+
+sqs_management_instance = SQSManagement()
+ec2_auto_scale_instance = EC2AutoScaling()
+
+app_sqs_resource = boto3.resource("sqs", region_name=constants.REGION_NAME)
+app_sqs_client = boto3.client('sqs', region_name=constants.REGION_NAME)
+
+response_queue_url = sqs_management_instance.get_queue_url(SQS_RESPONSE_QUEUE_NAME)
+request_queue_url = sqs_management_instance.get_queue_url(SQS_REQUEST_QUEUE_NAME)
+
+
+def get_message(queue_url):
+    try:
+        sqs_response = app_sqs_client.receive_message(
+            QueueUrl=queue_url,
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=['All'],
+            VisibilityTimeout=8,
+            WaitTimeSeconds=20
+        )
+        message = sqs_response.get('Messages', None)
+        return message[0] if message else None
+    except ClientError as e:
+        logging.error(e)
+
+
+def send_message_to_queue_response(queue_url, image_name):
+    try:
+        response = app_sqs_client.send_message(QueueUrl=queue_url,
+                                               MessageBody=(image_name))
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def delete_message_request(queue_url, receipt_handle):
+    try:
+        app_sqs_client.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle
+                                      )
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def store_image_to_s3(file_name, bucket_name, image_file):
+    try:
+        s3_client = boto3.client('s3')
+        response = s3_client.upload_file(file_name, bucket_name, image_file)
+    except ClientError as e:
+        logging.error(e)
+
+
+def write_to_file(image_name, result):
+    with open(image_name, "wb") as f:
+        f.write(bytes(result, 'utf8'))
+        f.close()
+
+
+def save_result_file_into_bucket(file_name, bucket_name, object_name):
+    try:
+        s3_client = boto3.client('s3')
+        response = s3_client.upload_file(file_name, bucket_name, object_name)
+    except ClientError as e:
+        logging.error(e)
+
+
+def shutting_down_instances():
+    instance_ids = ec2_auto_scale_instance.get_instances_by_state()
+    ec2_auto_scale_instance.stop_instances(instance_ids)
+    return None
+
+
+def get_image_after_decoding_base64(msg_value):
+    return base64.decodestring(msg_value)
+
+
+def get_output_from_classification(image_file_jpg):
+    return None
+
+
+def running_app_tier():
+    while True:
+        ec2_auto_scale_instance.auto_scale_instances()
+        if sqs_management_instance.numberOfMessagesInQueue(SQS_RESPONSE_QUEUE_NAME):
+            break
+        message = get_message(sqs_management_instance.get_queue_url(SQS_REQUEST_QUEUE_NAME))
+        if message is None:
+            break
+        msg_filename_key = message.get('key')
+        msg_base64_encoded_value = message.get('value')
+        transient_binary_file = msg_filename_key
+        image_file_jpg = get_image_after_decoding_base64(msg_base64_encoded_value)
+        store_image_to_s3(msg_filename_key, S3_INPUT_BUCKET, image_file_jpg)
+        classified_predicted_result = get_output_from_classification(image_file_jpg)
+        key_value_pair_predicted = '({0}, {1})'.format(msg_filename_key, classified_predicted_result)
+        write_to_file(transient_binary_file, key_value_pair_predicted)
+        save_result_file_into_bucket(transient_binary_file, S3_OUTPUT_BUCKET, transient_binary_file)
+        os.remove(transient_binary_file)
+        send_message_to_queue_response(sqs_management_instance.get_queue_url(SQS_REQUEST_QUEUE_NAME), msg_filename_key)
+        # deleting message after the message response is sent to queue
+        delete_message_request(sqs_management_instance.get_queue_url(SQS_REQUEST_QUEUE_NAME), message['ReceiptHandle'])
+    return None
+
+if __name__ == '__main__':
+    running_app_tier()
+    shutting_down_instances()
