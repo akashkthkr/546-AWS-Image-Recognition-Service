a4ddeb3e80b7c43fd6996480a14cf195dfbc22e2
Ayush Kalani <kalani.ayush@gmail.com>
2022-03-16 18:57:43 -0700

    logging payload

--- webTier.py
+++ webTier.py
@@ -1,6 +1,5 @@
 from quart import Quart, request, jsonify
 from PIL import Image
-# import time
 from time import sleep
 import base64
 import asyncio
@@ -8,18 +7,24 @@ import boto3
 import json
 import constants
 from SQSManagement import *
+from logging.config import dictConfig
 
+dictConfig({
+    'version': 1,
+    'loggers': {
+        'quart.app': {
+            'level': 'DEBUG',
+        },
+    },
+})
 REQUEST_QUEUE_NAME = constants.AWS_SQS_REQUEST_QUEUE_NAME
 RESPONSE_QUEUE_NAME = constants.AWS_SQS_RESPONSE_QUEUE_NAME
-
 
 async def async_get_data(msg):
     sleep(1)
     return str(msg)
 
-
 app = Quart(__name__)
-
 
 @app.route('/webTier', methods=['POST'])
 async def webTier():
@@ -29,16 +34,19 @@ async def webTier():
     value = str(value, 'utf-8')
 
     # create message
-    msg = {'key': key, 'value': value}
-    json_msg = json.dumps(msg)
+    json_msg = json.dumps({'key': key, 'value': value})
+    app.logger.debug("Request processed with payload %s", json_msg)
 
     # send message to SQS
-    queueUrl = get_queue_url(queue_name=REQUEST_QUEUE_NAME)
-    send_message(queueUrl, json_msg)
+    send_message(get_queue_url(), json_msg)
 
     # receive message from SQS
     return await async_get_data(json_msg)
-
+  
+@app.route('/health', methods=['GET'])
+def healthcheck():
+  app.logger.debug('OK')
+  return "200"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=6060)
