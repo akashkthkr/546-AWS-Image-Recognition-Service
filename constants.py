b72a8a0e20c565a3b6be7fa843a48247371d5b8a
Akash Kant <thakur.akashkant@gmail.com>
2022-03-16 20:10:02 -0700

    Constant with s3 name

--- constants.py
+++ constants.py
@@ -2,15 +2,15 @@ REGION_NAME = "us-east-1"
 SECURITY_GROUP_ID = ["sg-0bb98ead470e1d287"]
 APP_TIER_AMI = "ami-0a55e620aa5c79a24"
 INSTANCE_TYPE = 't2.micro'
-AWS_S3_INPUT_BUCKET_NAME = "input-bucket"
-AWS_S3_OUTPUT_BUCKET_NAME = "output-bucket"
+AWS_S3_INPUT_BUCKET_NAME = "cc-546-grp-11-input-bucket"
+AWS_S3_OUTPUT_BUCKET_NAME = "cc-546-grp-11-output-bucket"
 AWS_SQS_REQUEST_QUEUE_NAME = "images-requests"
 AWS_SQS_RESPONSE_QUEUE_NAME = "images-responses"
 
 ## SWAG
 STARTUP_BANNER =f"""
  +-+ +-+ +-+ +-+ +-+   +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+
- |C| |l| |o| |u| |d|   |c| |o| |m| |p| |u| |t| |i| |n| |g|
+ |C| |l| |o| |u| |d|   |C| |o| |m| |p| |u| |t| |i| |n| |g|
  +-+ +-+ +-+ +-+ +-+   +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+
 """
 
