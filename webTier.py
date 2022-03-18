387bbcd395016c1863f0081f148a5bfcf3cdd079
Akash Kant <thakur.akashkant@gmail.com>
2022-03-18 04:01:01 -0700

    Adding deleting key

--- webTier.py
+++ webTier.py
@@ -45,6 +45,7 @@ async def get_result(key):
         if key in result_dict:
             output_to_be_returned = '({0}, {1})'.format(key, result_dict[key])
             print("WebTier Output returned" + str(output_to_be_returned))
+            del result_dict[key]
             return output_to_be_returned
         
         
