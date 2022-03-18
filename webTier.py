be5af01321e1a89c2af2a982cd6f3e0f117d1469
nakulvaidya <vaidyanakul15@gmail.com>
2022-03-17 23:15:04 -0700

    collect and return response in webTier

--- webTier.py
+++ webTier.py
@@ -17,30 +17,45 @@ dictConfig({
         },
     },
 })
+
+result_dict = {}
+
 REQUEST_QUEUE_NAME = constants.AWS_SQS_REQUEST_QUEUE_NAME
 RESPONSE_QUEUE_NAME = constants.AWS_SQS_RESPONSE_QUEUE_NAME
 
 app = Quart(__name__)
 
-async def async_get_data(msg):
-    sleep(1)
-    return str(msg)
+
+async def collect_response():
+    while True:
+        queue_url = get_queue_url(constants.AWS_SQS_RESPONSE_QUEUE_NAME)
+        response = receive_message(queue_url)
+        for message in response.get("Messages", []):
+            message_body = message['Body']
+            message_dict = json.loads(message_body)
+            result_dict[message_dict['key']] = message_dict['value']
+            delete_message(queue_url, message['ReceiptHandle'])
+        sleep(1)
+
+
 
 @app.route('/classify-image', methods=['POST'])
 async def classify_image():
     file = (await request.files)['myfile']
     value = base64.b64encode(file.read())
     value = str(value, 'utf-8')
-
+    key = str(file.filename)
     # create message
-    json_msg = json.dumps({'key': str(file.filename), 'value': value})
+    json_msg = json.dumps({'key': key, 'value': value})
     app.logger.debug("Request processed with payload %s", json_msg)
 
     # send message to SQS
-    send_message(get_queue_url(), json_msg)
+    send_message(get_queue_url(constants.AWS_SQS_REQUEST_QUEUE_NAME), json_msg)
 
     # receive message from SQS
-    return await async_get_data(json_msg)
+    while not result_dict.has_key(key):
+        sleep(1)
+    return result_dict[key]
   
 @app.route('/health', methods=['GET'])
 def healthcheck():
@@ -51,3 +66,4 @@ if __name__ == '__main__':
     app.logger.info(constants.STARTUP_BANNER)
     app.logger.info(constants.STARTUP_BANNER_GROUP)
     app.run(host='0.0.0.0', debug=True, port=6060)
+    collect_response()
